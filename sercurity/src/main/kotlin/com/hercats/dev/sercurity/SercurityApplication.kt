5d1d921ce863db31cc0d3ef2430f39814045e13c
chengpiao <991146900@qq.com>
2018-09-14 10:55:00 +0800

    添加安全模块

--- sercurity/src/main/kotlin/com/hercats/dev/sercurity/SercurityApplication.kt
+++ sercurity/src/main/kotlin/com/hercats/dev/sercurity/SercurityApplication.kt
@@ -1,11 +1,11 @@
-package com.hercats.dev.security
+package com.hercats.dev.sercurity
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SecurityApplication
+class SercurityApplication
 
 fun main(args: Array<String>) {
-    runApplication<SecurityApplication>(*args)
+    runApplication<SercurityApplication>(*args)
 }
