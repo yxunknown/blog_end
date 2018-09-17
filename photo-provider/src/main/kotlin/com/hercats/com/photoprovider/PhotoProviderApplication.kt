716ac65f0464442a5478e5e16b937ce1e4c83a6d
chengpiao <991146900@qq.com>
2018-09-17 16:46:03 +0800

    photo controller

--- photo-provider/src/main/kotlin/com/hercats/com/photoprovider/PhotoProviderApplication.kt
+++ photo-provider/src/main/kotlin/com/hercats/com/photoprovider/PhotoProviderApplication.kt
@@ -4,11 +4,10 @@ import org.mybatis.spring.annotation.MapperScan
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
-import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
 @EnableEurekaClient
-@ComponentScan(value = ["com.hercats.dev"])
+//@ComponentScan(value = ["com.hercats.dev"])
 @MapperScan(value = ["com.hercats.dev.commonbase.mapper"])
 class PhotoProviderApplication
 
